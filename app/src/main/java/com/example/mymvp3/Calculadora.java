f13eac5b04a6aca5e17791478f3ec97e902d6ecf
slither366 <slither366@gmail.com>
2020-04-20 19:38:18 -0500

    Primera configuración inicial MVP ejemplo 3

---
+++ app/src/main/java/com/example/mymvp3/Calculadora.java
@@ -0,0 +1,14 @@
+package com.example.mymvp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculadora);
+    }
+}
